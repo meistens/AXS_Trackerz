a3f03378dbb45efbd844d1058923a4efe88dad53
David Mebo <krankd@proton.me>
2025-06-12 00:27:12 +0100

    more stuff added, forgot to track...again

--- internal/data/wallet/structs.go
+++ internal/data/wallet/structs.go
@@ -47,31 +47,37 @@ type Transactions struct {
 	//Verified                 int              `json:"verified"`
 	//Operator                 string           `json:"operator"`
 	//VerifiedCollection       bool             `json:"verified_collection"`
-	FromAddressEntity        *string          `json:"from_address_entity,omitempty"`
-	FromAddressEntityLogo    *string          `json:"from_address_entity_logo,omitempty"`
-	FromAddressLabel         *string          `json:"from_address_label,omitempty"`
-	ToAddressEntity          *string          `json:"to_address_entity,omitempty"`
-	ToAddressEntityLogo      *string          `json:"to_address_entity_logo,omitempty"`
-	ToAddress                string           `json:"to_address"`
-	ToAddressLabel           *string          `json:"to_address_label,omitempty"`
-	Value                    string           `json:"value"`
-	Gas                      string           `json:"gas"`
-	GasPrice                 string           `json:"gas_price"`
-	ReceiptCumulativeGasUsed string           `json:"receipt_cumulative_gas_used"`
-	ReceiptGasUsed           string           `json:"receipt_gas_used"`
-	ReceiptContractAddress   *string          `json:"receipt_contract_address,omitempty"`
-	ReceiptStatus            string           `json:"receipt_status"`
-	BlockTimestamp           string           `json:"block_timestamp"`
-	BlockNumber              string           `json:"block_number"`
-	BlockHash                string           `json:"block_hash"`
-	TransactionFee           string           `json:"transaction_fee"`
-	MethodLabel              *string          `json:"method_label,omitempty"`
-	NFTTransfers             []NFTTransfer    `json:"nft_transfers"`
-	ERC20Transfers           []ERC20Transfer  `json:"erc20_transfers"`
-	NativeTransfers          []NativeTransfer `json:"native_transfers"`
-	Summary                  string           `json:"summary"`
-	PossibleSpam             bool             `json:"possible_spam"`
-	Category                 string           `json:"category"`
+	FromAddressEntity        *string               `json:"from_address_entity,omitempty"`
+	FromAddressEntityLogo    *string               `json:"from_address_entity_logo,omitempty"`
+	FromAddressLabel         *string               `json:"from_address_label,omitempty"`
+	ToAddressEntity          *string               `json:"to_address_entity,omitempty"`
+	ToAddressEntityLogo      *string               `json:"to_address_entity_logo,omitempty"`
+	ToAddress                string                `json:"to_address"`
+	ToAddressLabel           *string               `json:"to_address_label,omitempty"`
+	Value                    string                `json:"value"`
+	Gas                      string                `json:"gas"`
+	GasPrice                 string                `json:"gas_price"`
+	ReceiptCumulativeGasUsed string                `json:"receipt_cumulative_gas_used"`
+	ReceiptGasUsed           string                `json:"receipt_gas_used"`
+	ReceiptContractAddress   *string               `json:"receipt_contract_address,omitempty"`
+	ReceiptStatus            string                `json:"receipt_status"`
+	BlockTimestamp           string                `json:"block_timestamp"`
+	BlockNumber              string                `json:"block_number"`
+	BlockHash                string                `json:"block_hash"`
+	TransactionFee           string                `json:"transaction_fee"`
+	MethodLabel              *string               `json:"method_label,omitempty"`
+	NFTTransfers             []NFTTransfer         `json:"nft_transfers"`
+	ERC20Transfers           []ERC20Transfer       `json:"erc20_transfers"`
+	NativeTransfers          []NativeTransfer      `json:"native_transfers"`
+	Summary                  string                `json:"summary"`
+	PossibleSpam             bool                  `json:"possible_spam"`
+	Category                 string                `json:"category"`
+	InternalTransactions     []InternalTransaction `json:"internal_transactions,omitempty"`
+}
+
+type InternalTransaction struct {
+	// if anything new pops up while exploring API, worthwhile or not, add
+	// who knows when it will be useful...
 }
 
 type ERC20Transfer struct {
@@ -98,25 +104,49 @@ type ERC20Transfer struct {
 }
 
 type NFTTransfer struct {
-	LogIndex              int     `json:"log_index"`
-	Value                 string  `json:"value"`
-	ContractType          string  `json:"contract_type"`
-	TransactionType       string  `json:"transaction_type"`
-	TokenAddress          string  `json:"token_address"`
-	TokenID               string  `json:"token_id"`
-	FromAddressEntity     *string `json:"from_address_entity,omitempty"`
-	FromAddressEntityLogo *string `json:"from_address_entity_logo,omitempty"`
-	FromAddress           string  `json:"from_address"`
-	FromAddressLabel      *string `json:"from_address_label,omitempty"`
-	ToAddressEntity       *string `json:"to_address_entity,omitempty"`
-	ToAddressEntityLogo   *string `json:"to_address_entity_logo,omitempty"`
-	ToAddress             string  `json:"to_address"`
-	ToAddressLabel        *string `json:"to_address_label,omitempty"`
-	Amount                string  `json:"amount"`
-	Operator              string  `json:"operator"`
-	PossibleSpam          bool    `json:"possible_spam"`
-	VerifiedCollection    bool    `json:"verified_collection"`
-	Direction             string  `json:"direction"`
+	LogIndex              int                 `json:"log_index"`
+	Value                 string              `json:"value"`
+	ContractType          string              `json:"contract_type"`
+	TransactionType       string              `json:"transaction_type"`
+	TokenAddress          string              `json:"token_address"`
+	TokenID               string              `json:"token_id"`
+	FromAddressEntity     *string             `json:"from_address_entity,omitempty"`
+	FromAddressEntityLogo *string             `json:"from_address_entity_logo,omitempty"`
+	FromAddress           string              `json:"from_address"`
+	FromAddressLabel      *string             `json:"from_address_label,omitempty"`
+	ToAddressEntity       *string             `json:"to_address_entity,omitempty"`
+	ToAddressEntityLogo   *string             `json:"to_address_entity_logo,omitempty"`
+	ToAddress             string              `json:"to_address"`
+	ToAddressLabel        *string             `json:"to_address_label,omitempty"`
+	Amount                string              `json:"amount"`
+	Operator              string              `json:"operator"`
+	PossibleSpam          bool                `json:"possible_spam"`
+	VerifiedCollection    bool                `json:"verified_collection"`
+	Direction             string              `json:"direction"`
+	CollectionLogo        string              `json:"collection_logo"`
+	CollectionBannerImage string              `json:"collection_banner_image"`
+	NormalizedMetadata    *NormalizedMetadata `json:"normalized_metadata,omitempty"`
+}
+
+type NormalizedMetadata struct {
+	Name         string      `json:"name"`
+	Description  string      `json:"description"`
+	AnimationURL *string     `json:"animation_url,omitempty"`
+	ExternalLink *string     `json:"external_link,omitempty"`
+	Image        string      `json:"image"`
+	Attributes   []Attribute `json:"attributes"`
+}
+
+type Attribute struct {
+	TraitType   string   `json:"trait_type"`
+	Value       any      `json:"value"` // Can be string or number
+	DisplayType *string  `json:"display_type,omitempty"`
+	MaxValue    *int     `json:"max_value,omitempty"`
+	TraitCount  int      `json:"trait_count"`
+	Order       *int     `json:"order,omitempty"`
+	RarityLabel *string  `json:"rarity_label,omitempty"`
+	Count       *int     `json:"count,omitempty"`
+	Percentage  *float64 `json:"percentage,omitempty"`
 }
 
 type NativeTransfer struct {
